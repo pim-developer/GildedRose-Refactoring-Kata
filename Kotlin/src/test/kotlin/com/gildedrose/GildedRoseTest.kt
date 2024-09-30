4880dd94fa0f082432713edef8df52d7099d5307
Pim Dhaen <dhaen.dev@gmail.com>
2024-09-30 08:26:55 +0200

    Add unit test for GildedRose initial items
    - Verifies that GildedRose.items returns the items passed in constructor
    - Checks item count, names, sellIn, and quality values

--- Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -6,12 +6,21 @@ import org.junit.jupiter.api.Test
 internal class GildedRoseTest {
 
     @Test
-    fun foo() {
-        val items = listOf(Item("foo", 0, 0))
-        val app = GildedRose(items)
-        app.updateQuality()
-        assertEquals("fixme", app.items[0].name)
+    fun `Test items can be retrieved, and in the right order`() {
+        val expectedItems = listOf(
+            Item("foo", 0, 0),
+            Item("bar", 0, 0)
+        )
+        val app = GildedRose(expectedItems)
 
+        val actualItems = app.items
+        assertEquals(expectedItems.size, actualItems.size, "The number of items should match")
+        expectedItems.forEachIndexed { index, expectedItem ->
+            val actualItem = actualItems[index]
+            assertEquals(expectedItem.name, actualItem.name, "Item name at index $index should match")
+            assertEquals(expectedItem.sellIn, actualItem.sellIn, "Item sellIn at index $index should match")
+            assertEquals(expectedItem.quality, actualItem.quality, "Item quality at index $index should match")
+        }
     }
 
 }
